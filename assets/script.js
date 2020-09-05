d2746078bbc458a071384a21a83717e6c7343991
Natalia <nflores.teoria@gmail.com>
2020-09-05 18:00:30 -0400

    Funcionando consulta de nombre, habilidad y gráfico

--- assets/script.js
+++ assets/script.js
@@ -13,6 +13,7 @@ $( document ).ready(function() {
                 url: `https://pokeapi.co/api/v2/pokemon/${pokemon}`,
                 success: function (response) {
                     $(".devolucion").text(`${response.name}!`);
+                    $(".devolucion2").html(`<img src="${response.sprites.front_default}" style="width:30%"> <img src="${response.sprites.back_default}">`);
                     let abilities = response.abilities;
                     let table = `
                         <table class="table">
@@ -22,7 +23,7 @@ $( document ).ready(function() {
                                 </tr>
                             </thead>
                             <tbody>
-                    `;
+                        `;
                     abilities.forEach(element => {
                         table += `
                                 <tr>
@@ -34,7 +35,9 @@ $( document ).ready(function() {
                             </tbody>
                         </table>                        
                     `;
-                    $(`.devolucion2`).append(table);
+                    $(`.devolucion3`).html(table);
+                    graph(response);
+                    // $(`#chartContainer`).text(graph(response));
                     //Habilidad sin tabla: $(".devolucion2").append(`<p>${element.ability.name}</p>`);
                 },
                 error: function (error) {
@@ -47,26 +50,30 @@ $( document ).ready(function() {
         }
     });
 
-    //Gráfico
+//Gráfico
+function graph (pok) {
+    let datos = [];
+
+    pok.stats.forEach(element => {
+        datos.push({
+            label: element.stat.name, 
+            y: element.base_stat
+        });
+    });
 
     var options = {
         title: {
             text: "Stats de mi Pokemón"              
         },
-        data: [              
-        {
+        data: [
+            {
             type: "column",
-            dataPoints: [
-                { label: "apple",  y: 10  },
-                { label: "orange", y: 15  },
-                { label: "banana", y: 25  },
-                { label: "mango",  y: 30  },
-                { label: "grape",  y: 28  }
-            ]
-        }
+            dataPoints: datos                    
+            }
         ]
-    };
+    };     
+    //Carga la variable con el gráfico   
+    $("#chartContainer").CanvasJSChart(options);    
+};
 
-    //Carga la variable con el gráfico
-    $("#chartContainer").CanvasJSChart(options);
 });
